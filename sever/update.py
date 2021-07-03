fe31283bafd426e4e768e54aaf0aba143dd2b7f9
Hoang Cao Bao <62338248+hoangcaobao@users.noreply.github.com>
2021-07-03 11:23:26 +0700

    Update update.py

--- sever/update.py
+++ sever/update.py
@@ -2,6 +2,9 @@ import requests
 from sever import link
 import pandas as pd
 import threading
+from sever import events_rds, orgs_rds
+from collaborative_filtering.events import CF_events
+from collaborative_filtering.orgs import CF_orgs
 #function to get graphql
 
 def run_query(query):
@@ -25,7 +28,7 @@ def update():
     #query sql api
     events_query = """
     {
-    events{
+    events(limit: 0){
         id
         name
         categories
@@ -36,7 +39,7 @@ def update():
     events=events['data']['events']
     orgs_query = """
     {
-    orgs{
+    orgs(limit:0){
         id
         name
         categories
@@ -95,3 +98,14 @@ def update():
     #create pandas framework from dic then save it to category
     df=pd.DataFrame(data=dic)
     df.to_csv('database/orgs_category.csv',index=False)
+
+    #reset
+    events_clicking=pd.read_csv("database/events_clicking.csv", encoding="latin-1")
+    events_category=pd.read_csv("database/events_category.csv", encoding="latin-1")
+    events_rds=CF_events(events_clicking,events_category,2,8)
+    events_rds.preprocessing_data()   
+    orgs_clicking=pd.read_csv("database/orgs_clicking.csv", encoding="latin-1")
+    orgs_category=pd.read_csv("database/orgs_category.csv", encoding="latin-1")
+    orgs_rds=CF_orgs(orgs_clicking,orgs_category,2,8)
+    orgs_rds.preprocessing_data()  
+
